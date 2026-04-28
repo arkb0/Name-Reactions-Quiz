961bfc04208fb0d5d34f39987b64e9512002fa57
arkb0 <armaghankhanbajwa@gmail.com>
2026-04-28 05:55:15 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // If we are in GitHub Actions, use the repo name; otherwise, use root
+  base: process.env.NODE_ENV === 'production' 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` 
+    : '/',
+  
   plugins: [
     vue(),
     vueJsx(),
